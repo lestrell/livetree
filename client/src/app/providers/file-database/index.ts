415cef43f96fad7f5586fd99914b808ca2f8505c
Lucas Estrella <sir.estrella@gmail.com>
2018-08-11 18:51:40 -0400

    Refactor file-database

--- client/src/app/providers/file-database/index.ts
+++ client/src/app/providers/file-database/index.ts
@@ -4,33 +4,36 @@ import { MatTree } from '@angular/material';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import {BehaviorSubject} from 'rxjs';
+import { isArray, map } from 'lodash';
 
 /**
  * Json node data with nested structure. Each node has a filename and a value or a list of children
  */
-export class FileNode {
-  children: FileNode[];
-  filename: string;
-  type: any;
+export interface FileNode {
+  children?: FileNode[];
+  // filename?: string;
+  // type?: any;
+  key?: string;
 }
 
 /** Flat node with expandable and level information */
 export class FileFlatNode {
-  filename: string;
-  type: any;
+  // filename: string;
+  // type: any;
+  key: string;
   level: number;
   expandable: boolean;
 }
 
 
-/**
- * The Json tree data in string. The data could be parsed into Json object
- */
-const TREE_DATA = JSON.stringify({
-  Root: {
-    angular: [ 'src' ]
-  }
-});
+// /**
+//  * The Json tree data in string. The data could be parsed into Json object
+//  */
+// const TREE_DATA = JSON.stringify({
+//   Root: {
+//     angular: [0, 1, 2, 3, 4, "Lucas"]
+//   }
+// });
 
 
 /**
@@ -46,22 +49,27 @@ export class FileDatabase {
 
   get data(): FileNode[] { return this.dataChange.value; }
 
-  constructor() {
-    this.initialize();
+  get rootChildren(): FileNode[] {
+    return this.data[0].children;
   }
 
-  initialize() {
-    // Parse the string to json object.
-    const dataObject = JSON.parse(TREE_DATA);
+  refresh = () => this.dataChange.next(this.data);
 
-    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
-    //     file node as children.
-    const data = this.buildFileTree(dataObject, 0);
-    // console.log(data);
+  constructor() {
+
+  }
+
+  pushRootChild(child: FileNode) {
+    this.rootChildren.push(child);
+    this.refresh();
+  }
+
+  refreshData<T>(dataObject: {[key: string]: T}) {
+    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested file node as children.
+    const data = this.buildFileTree({ Root: dataObject }, 0);
 
     // Notify the change.
     this.dataChange.next(data);
-
   }
 
   /**
@@ -71,15 +79,18 @@ export class FileDatabase {
   buildFileTree(obj: object, level: number): FileNode[] {
     return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
       const value = obj[key];
-      const node = new FileNode();
-      node.filename = key;
+      // const node: FileNode = { filename: key };
+      const node: FileNode = { key };
 
       if (value != null) {
-        if (typeof value === 'object') {
+        if (isArray(value)) {
+          node.children = map(value, _key => ({key: _key}));
+        } else if (typeof value === 'object') {
           node.children = this.buildFileTree(value, level + 1);
-        } else {
-          node.type = value;
         }
+        // else {
+        //   node.name = value;
+        // }
       }
 
       return accumulator.concat(node);
